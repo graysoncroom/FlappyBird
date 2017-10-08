af0741a3141f0bdbb1aec50cf27ffe48f18a0b56
Grayson Croom <grayson.croom@gmail.com>
2017-10-08 14:41:35 -0500

    Added main character (the bird) and basic physics.

--- core/src/com/graysoncroom/flappybird/FlappyBird.java
+++ core/src/com/graysoncroom/flappybird/FlappyBird.java
@@ -8,26 +8,55 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+
+    Texture[] birds;
+    int currentBirdIndex = 0;
+    float birdY = 0;
+    float velocity = 0;
+
+    int gameState = 0;
+    float gravity = 3/2;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+
+        birds = new Texture[2];
+	    birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+
+        birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+
+        if (gameState != 0) {
+            if (Gdx.input.justTouched()) {
+                velocity = -20;
+            }
+
+            if (birdY > 0 || velocity < 0) {
+                velocity += gravity;
+                birdY -= velocity;
+            }
+
+        } else if (Gdx.input.justTouched()) {
+            gameState = 1;
+        }
+
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Texture currentBird = birds[getCurrentBirdIndex()];
+        batch.draw(currentBird, Gdx.graphics.getWidth() / 2 - currentBird.getWidth() / 2, birdY);
+        batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+	public int getCurrentBirdIndex() {
+        int index = currentBirdIndex;
+        currentBirdIndex = index == 0 ? 1 : 0;
+        return index;
+    }
 }
